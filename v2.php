47004030f0143c4fac4d9ce22f7b2e7301f1338b
Registro de Medios Informáticos <38992498+manueldej@users.noreply.github.com>
2023-02-03 10:28:43 -0500

    Update v2.php

--- v2.php
+++ v2.php
@@ -1,13 +1,13 @@
 <?php 
 ############################################################################################################
 # Software: Regimed                                                                                        #
-#(Registro de Medios Informáticos)     					                                		           #
-# Version:  3.0.1                                                     				                       #
-# Fecha:    01/06/2016 - 03/04/2018                                             					                       #
-# Autores:  Ing. Manuel de Jesús Núñez Guerra   								     			           #
-#          	Msc. Carlos Pollan Estrada											         		           #
-# Licencia: Freeware                                                				                       #
-#                                                                       			                       #
+#(Registro de Medios Informáticos)     					                                   #
+# Version:  3.0.1                                                     				           #
+# Fecha:    01/06/2016 - 03/04/2018                                             			   #
+# Autores:  Ing. Manuel de Jesús Núñez Guerra   							   #
+#           Msc. Carlos Pollan Estrada									   #
+# Licencia: Freeware                                                				           #
+#                                                                       			           #
 # Usted puede usar y modificar este software si asi lo desea, pero debe mencionar la fuente                #
 ############################################################################################################
 include('header.php');
